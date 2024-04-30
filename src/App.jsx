49c6bbd60c5455fb1f8e572e34b13d7ed8aba204
Felix <felixneumayr@gmx.de>
2024-04-30 16:46:07 +0200

    add visual delete button and followup question

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,26 @@ import "./App.css";
 import ColorForm from "./Components/Color/form/ColorForm";
 import { useState } from "react";
 import { uid } from "uid";
+import { DeleteButton } from "./Components/Color/color/Color";
 
 function App() {
   const [colors, setColors] = useState(initialColors);
 
-  function handleSubmitColor(data){
-    setColors([{id: uid(), ...data}, ...colors]);
-
+  function handleSubmitColor(data) {
+    setColors([{ id: uid(), ...data }, ...colors]);
   }
+  
+  // function handleDelete(){
+  //   console.log("wird gelöscht");
+  // }   wie gehts weiter, wenn ich auf delete und delete bestätigen drücke?
+
+
 
 
   return (
     <>
       <h1>Theme Creator</h1>
-      <ColorForm onSubmitColor={handleSubmitColor}/>
+      <ColorForm onSubmitColor={handleSubmitColor} />
       {colors.map((color) => (
         <Color key={color.id} color={color} />
       ))}
--- src/Components/Color/color/Color.jsx
+++ src/Components/Color/color/Color.jsx
@@ -1,4 +1,5 @@
 import "./Color.css";
+import { useState } from "react";
 
 export default function Color({ color }) {
   return (
@@ -12,6 +13,43 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      <DeleteButton  />
     </div>
   );
 }
+
+export function DeleteButton() {
+  const [remove, setRemove] = useState(false);
+
+  function handleClick() {
+    setRemove(true);
+    console.log(remove);
+  }
+
+  
+
+
+function handleDecision(event){
+event.target.id === "cancel" ? setRemove(false) : console.log("wird gelöscht") /* ??? */;
+}
+
+
+
+  return (
+    <div>
+      {remove ? (
+        <div>
+          <p>Are you sure you want to delete?</p>
+          <button id="cancel" onClick={handleDecision}>Cancel</button>
+          <button id="delete" onClick={handleDecision}>Delete</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={handleClick}>Delete</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+//
